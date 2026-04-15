aed267f050c2e3d30e7f7e32f1c9f589967232fa
davizeds <davinazario1234@hotmail.com>
2026-04-15 17:36:38 -0300

    feat: add stats endpoint and refactor file categorization logic

---
+++ app/categorizacao.py
@@ -0,0 +1,38 @@
+import os
+from app.dicionario import categorias
+
+
+
+
+
+def categorizador(caminho):
+    arquivos_encontrados = {'audios': [], 'videos': [], 'imagens': [], 'executaveis': [], 'documentos': [],
+                            'arquivos_compactados': [], 'scripts': []}
+    arquivos = os.listdir( caminho )
+    total_arquivos=0
+    for arc in arquivos:
+        if os.path.isfile( os.path.join(caminho, arc ) ):
+            total_arquivos += 1
+            arquivo_separado = arc.split( sep='.' )
+            extensao = arquivo_separado[-1].lower()
+            if extensao in categorias['audios']:
+                arquivos_encontrados['audios'].append( arc )
+
+            elif extensao in categorias['videos']:
+                arquivos_encontrados['videos'].append( arc )
+
+            elif extensao in categorias['imagens']:
+                arquivos_encontrados['imagens'].append( arc )
+
+            elif extensao in categorias['executaveis']:
+                arquivos_encontrados['executaveis'].append( arc )
+
+            elif extensao in categorias['documentos']:
+                arquivos_encontrados['documentos'].append( arc )
+
+            elif extensao in categorias['arquivos_compactados']:
+                arquivos_encontrados['arquivos_compactados'].append( arc )
+
+            elif extensao in categorias['scripts']:
+                arquivos_encontrados['scripts'].append( arc )
+    return  arquivos_encontrados, total_arquivos
---
+++ app/dicionario.py
@@ -0,0 +1,20 @@
+categorias = {
+    'audios': ['mp3', 'wav', 'aac', 'flac', 'ogg', 'wma', 'm4a', 'aiff', 'alac', 'opus',
+               'amr', 'mid', 'midi', 'ra', 'dsd', 'ape'],
+
+    'videos': ['mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', 'mpeg', 'mpg', '3gp',
+               'm4v', 'vob', 'ts', 'ogv', 'f4v'],
+
+    'imagens': ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'tif', 'webp', 'heic', 'svg',
+                'raw', 'psd', 'ico', 'avif', 'jfif'],
+
+    'executaveis': ['exe', 'msi', 'bat', 'cmd', 'sh', 'jar', 'apk', 'app', 'deb',
+                    'rpm', 'bin'],
+
+    'documentos': ['txt', 'doc', 'docx', 'odt', 'pdf', 'rtf', 'tex', 'log', 'md', 'csv',
+                   'json', 'xml', 'yaml', 'yml', 'xlsx', 'xls', 'ppt', 'pptx', 'ods', 'odp'],
+
+    'arquivos_compactados': ['zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'iso', 'dmg'],
+
+    'scripts': ['py', 'js', 'html', 'css', 'java', 'c', 'cpp', 'cs', 'rb', 'php', 'go',
+                'rs', 'swift', 'htm']}
