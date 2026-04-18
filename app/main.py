ea64ccfeaadd5e3c893fde6949c2f8ff8fbff3a6
davizeds <davinazario1234@hotmail.com>
2026-04-18 17:54:01 -0300

    feat:  add file filters by name, date and minimum size

--- app/main.py
+++ app/main.py
@@ -18,22 +18,36 @@ async def rota():
 
 
 @app.get('/files')
-def lista_arquivos(caminho:str,extensao: Optional[str] = None):
+def lista_arquivos(caminho:str,extensao: Optional[str] = None, nome: Optional[str] = None,dia: Optional[int] = None,
+                   mes: Optional[int] = None,ano: Optional[int] = None,tamanho_minimo: Optional[int] = None):
     if not os.path.exists(caminho):
         raise HTTPException( status_code=404, detail='esse caminho nao existe' )
     arquivos = os.listdir( caminho )
     lugares={'arquivos':[],'pastas':[]}
     for itens in arquivos:
         caminho_completo=os.path.join(caminho,itens)
-        if os.path.isfile(caminho_completo):
-            if extensao:
-                if itens.endswith('.'+extensao) :
-                    lugares['arquivos'].append(itens)
-            else:
-                lugares['arquivos'].append(itens)
+        if os.path.isfile( caminho_completo ):
+            pode_entrar = True
+            if nome and nome.lower() not in itens.lower():
+                    pode_entrar = False
+            if extensao and not itens.endswith('.'+extensao):
+                    pode_entrar = False
+            data_modificada = os.path.getmtime( caminho_completo )
+            data_convertida = datetime.fromtimestamp( data_modificada )
+            if dia and dia !=  data_convertida.day:
+                pode_entrar = False
+            if mes and mes != data_convertida.month:
+                pode_entrar = False
+            if ano and ano !=  data_convertida.year:
+                pode_entrar = False
+            tamanho=os.path.getsize(caminho_completo)
+            if tamanho_minimo and tamanho_minimo>tamanho:
+                pode_entrar = False
+            if pode_entrar:
+                lugares['arquivos'].append( itens )
         elif os.path.isdir(caminho_completo):
             lugares['pastas'].append(itens )
-    return lugares
+    return {'status': 'sucesso', 'caminho': caminho,'arquivos': lugares['arquivos'],'pastas':lugares['pastas']}
 
 
 
